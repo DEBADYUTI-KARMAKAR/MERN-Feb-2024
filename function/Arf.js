8213d7e45e77986db35236428fb1623dc6ef1067
Debadyuti <debadyuti.karmakar@gmail.com>
2024-02-18 18:13:08 +0530

    Code & notes

---
+++ function/Arf.js
@@ -0,0 +1,20 @@
+let Sum=()=>{
+    let a=10;
+    let b=5;
+    console.log(a+b);
+}
+Sum();
+let Sub=(a,b)=>{
+    let c = a-b;
+    return c;
+}
+let r=Sub(4,6);
+console.log(r);
+
+let Mult=(a,b)=>a*b;
+console.log(Mult(5,7));
+
+let Div=(a,b)=>(
+    a/b+(7)
+);
+console.log(Div(9,3));
---
+++ function/Fav.js
@@ -0,0 +1,13 @@
+let Sum=function(a,b){
+    console.log(a+b);
+}
+
+Sum(3,8);
+
+const Sub=function(a,b){
+    return a-b;
+}
+
+let res = Sub(3,8);
+console.log(res);
+console.log(typeof(Sub));
